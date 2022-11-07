839d05733bcce936227e17fc7eb916c77bb4a8f1
Stelios Magalios <stelios.magalios1@gmail.com>
2022-11-07 23:23:16 +0200

    Basic docker stuff. dev works

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const app = express();
+const client = new PrismaClient();
+
+app.get("/", async (req, res) => {
+  res.status(200).json(await client.example.findMany());
+});
+
+app.listen(4000, () => console.log("listening on port 4000"));
