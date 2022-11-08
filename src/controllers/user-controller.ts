31f55c44560d87cf9c2c6163513491e8963a528f
Stelios Magalios <stelios.magalios1@gmail.com>
2022-11-08 12:41:50 +0200

    implemented user-controller functions

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,9 +1,25 @@
+import { prisma } from "..";
+
 /**
  * @param id The id of the user
  * @returns The user's current balance
  */
 async function balance(id: number): Promise<number> {
-  return -1;
+  try {
+    // Find the user's account that matches the id
+    const acc = await prisma.account.findUnique({ where: { userId: id } });
+
+    // Check if the account exists
+    if (!acc) {
+      throw new Error("Account doesn't exist");
+    }
+
+    // Return the user's balance
+    return acc.balance;
+  } catch (err: any) {
+    // If an error occured during execution (most likely prisma) return a generic message
+    throw new Error("Error happened when calculating user's balance");
+  }
 }
 
 /**
@@ -14,7 +30,27 @@ async function balance(id: number): Promise<number> {
  * @throws Will throw an error if the user is not found
  */
 async function deposit(id: number, amount: number): Promise<number> {
-  return -1;
+  try {
+    // Find the user's account that matches the id
+    const acc = await prisma.account.findUnique({ where: { userId: id } });
+
+    // Check if the account exists
+    if (!acc) {
+      throw new Error("Account doesn't exist");
+    }
+
+    // Store the amount to the account
+    const updatedAcc = await prisma.account.update({
+      where: { id: acc.id },
+      data: { balance: acc.balance + amount },
+    });
+
+    // Return the user's balance
+    return updatedAcc.balance;
+  } catch (err: any) {
+    // If an error occured during execution (most likely prisma) return a generic message
+    throw new Error("Error happened during deposit");
+  }
 }
 
 /**
@@ -25,7 +61,32 @@ async function deposit(id: number, amount: number): Promise<number> {
  * @throws Will throw an error if the user is not found or the amount requested is greater than the user's balance
  */
 async function withdraw(id: number, amount: number): Promise<number> {
-  return -1;
+  try {
+    // Find the user's account that matches the id
+    const acc = await prisma.account.findUnique({ where: { userId: id } });
+
+    // Check if the account exists
+    if (!acc) {
+      throw new Error("Account doesn't exist");
+    }
+
+    // Check if the withdrawal can be made
+    if (amount > acc.balance) {
+      throw new Error("Insufficient funds in user's account");
+    }
+
+    // Withdraw the amount to the account
+    const updatedAcc = await prisma.account.update({
+      where: { id: acc.id },
+      data: { balance: acc.balance - amount },
+    });
+
+    // Return the user's balance
+    return updatedAcc.balance;
+  } catch (err: any) {
+    // If an error occured during execution (most likely prisma) return a generic message
+    throw new Error("Error happened during deposit");
+  }
 }
 
 export default {
